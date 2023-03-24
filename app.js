79ccb9848078d64093403572a0b0d4bd45b99ad7
Rony <rnicksonaxter@gmail.com>
2023-03-23 22:50:26 -0300

    first commit adding all the structure, app.js file, database connected and running. all minimun structure is developed

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+const allow = process.env.ALLOW_ORIGIN || "*";
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", '*');
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    next();
+  })
+  .use("/", require("./routes"));
+
+app.use(session({
+  secret: 'mysecret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to DB and listening on ${port}`);
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(`Mongoose connection error: ${err}`);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
---
+++ swagger.js
@@ -0,0 +1,21 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'My Sports API',
+    description: 'Description',
+  },
+  // host: ['final-project-cse-341.onrender.com'],
+  host: ['localhost:8080'],
+
+  schemes: ['https', 'http'],
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+/* NOTE: if you use the express Router, you must pass in the 
+   'endpointsFiles' only the root file where the route starts,
+   such as index.js, app.js, routes.js, ... */
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
