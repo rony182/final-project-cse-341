ec21ab26781a1a60f23e57ebe75c5e53b39feaad
Alirio Mieres <90535036+Alirio-Mieres@users.noreply.github.com>
2023-03-24 23:59:37 -0400

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const authRouter = require('./auth');
+const swagger = require('./swagger');
+const userRouter = require('./user');
 
+router.use('/users', userRouter);
+router.use('/auth', authRouter);
+router.use('/', swagger);
 
 module.exports = router;
