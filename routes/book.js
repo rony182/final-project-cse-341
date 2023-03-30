1304d354a02c1d31e8f2545040d67c855d43af94
Rony <rnicksonaxter@gmail.com>
2023-03-30 18:28:50 -0300

    fixing

--- routes/book.js
+++ routes/book.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const router = express.Router();
 const {bookValidator} = require('../helpers/book-valid')
-const validateJWT = require('../middlewares/validate-jwt');
 
 const booksControllers = require('../controllers/book')
 
 router.get('/:id', booksControllers.getBookById)
 router.get('/', booksControllers.getBooks)
-router.post('/', [bookValidator, validateJWT], booksControllers.createBook)
-router.delete('/:id',validateJWT, booksControllers.deleteBook)
-router.put('/:id', [bookValidator, validateJWT], booksControllers.updateBook)
+router.post('/', [bookValidator], booksControllers.createBook)
+router.delete('/:id', booksControllers.deleteBook)
+router.put('/:id', [bookValidator], booksControllers.updateBook)
 
 module.exports = router;
